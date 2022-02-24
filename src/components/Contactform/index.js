35c6e45ebdb5bef61ae3312a30de92ea793d25f5
lucianovictor <lucianocarvallho7@gmail.com>
2022-02-24 11:37:54 -0300

    :construction: Create Controlled Components in ContactForm

--- src/components/Contactform/index.js
+++ src/components/Contactform/index.js
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 
+import { useState } from 'react';
 import { Form, ButtonContainer } from './styles';
 
 import FormGroup from '../FormGroup';
@@ -8,10 +9,16 @@ import Select from '../Select';
 import Button from '../Button';
 
 export default function ContactFrom({ buttonLabel }) {
+  const [name, setName] = useState('');
+
   return (
     <Form>
       <FormGroup>
-        <Input placeholder="Nome" />
+        <Input
+          value={name}
+          placeholder="Nome"
+          onChange={(event) => setName(event.target.value)}
+        />
       </FormGroup>
 
       <FormGroup>
