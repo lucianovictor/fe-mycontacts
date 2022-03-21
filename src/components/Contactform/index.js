64abad0f3950407e71b14188c86b6bca031b0a17
Luciano <54756923+lucianovictor@users.noreply.github.com>
2022-03-21 12:55:54 -0300

    :sparkles: Getting the data and handling the form's submit and title

--- src/components/Contactform/index.js
+++ src/components/Contactform/index.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 
-import { useState, useRef } from 'react';
+import { useState } from 'react';
 import { Form, ButtonContainer } from './styles';
 
 import FormGroup from '../FormGroup';
@@ -10,30 +10,52 @@ import Button from '../Button';
 
 export default function ContactFrom({ buttonLabel }) {
   const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [category, setCategory] = useState('');
 
-  const emailInput = useRef(null);
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    console.log({
+      name, email, phone, category,
+    });
+  }
 
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <FormGroup>
         <Input
-          value={name}
           placeholder="Nome"
+          value={name}
           onChange={(event) => setName(event.target.value)}
         />
       </FormGroup>
 
       <FormGroup>
-        <Input placeholder="E-mail" ref={emailInput} />
+        <Input
+          placeholder="E-mail"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
       </FormGroup>
 
       <FormGroup>
-        <Input placeholder="Telefone" />
+        <Input
+          placeholder="Telefone"
+          value={phone}
+          onChange={(event) => setPhone(event.target.value)}
+        />
       </FormGroup>
 
       <FormGroup>
-        <Select>
+        <Select
+          value={category}
+          onChange={(event) => setCategory(event.target.value)}
+        >
+          <option value=""> Category</option>
           <option value="Instagram"> Instagram</option>
+          <option value="Discord"> Discord </option>
         </Select>
       </FormGroup>
 
